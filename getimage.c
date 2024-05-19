a2c4d24e14db3481b02c39e47a4109ddbcaddb65
bk <bartosz.michal.k@gmail.com>
2024-05-19 19:28:45 +0200

    add jpeg support

--- getimage.c
+++ getimage.c
@@ -148,6 +148,46 @@ static void save_image_to_bmp(const uint8_t *rgb, int width, int height)
     fclose(file);
 }
 
+static void save_jpeg(const char *filename, uint8_t *rgb, int width, int height)
+{
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    FILE *outfile;
+    JSAMPROW row_pointer[1];
+    int row_stride;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    if ((outfile = fopen(filename, "wb")) == NULL) {
+        fprintf(stderr, "can't open %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 90, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    row_stride = width * 3;
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = &rgb[cinfo.next_scanline * row_stride];
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+}
+
 static void process_image(const void *p, int size)
 {
     if (out_buf) {
